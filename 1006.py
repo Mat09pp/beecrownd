3082f1153d360530a21eee4f710ba3008e93b728
Mat09pp <matheusladislau351@gmail.com>
2025-02-24 23:42:53 -0300

    feat: create 1006..py

---
+++ 1006.py
@@ -0,0 +1,10 @@
+A = float(input())
+B = float(input())
+C = float(input())
+
+nota1 = A * 2.0
+nota2 = B * 3.0
+nota3 = C * 5.0
+mediaf = (nota1 + nota2 + nota3) / 10.0
+
+print(f"MEDIA = {mediaf:.1f}")
