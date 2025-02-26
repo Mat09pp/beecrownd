01d427a558dac09ea002eff24150cf6753970445
Mat09pp <matheusladislau351@gmail.com>
2025-02-25 22:16:06 -0300

    feat: create 1008.py

---
+++ 1008.py
@@ -0,0 +1,8 @@
+funcionarios = int(input())
+horas_trab = int(input())
+dinheiro = float(input())
+
+salario = dinheiro * horas_trab
+
+print(f"NUMBER = {funcionarios}")
+print(f"SALARY = U$ {salario:.2f}")
